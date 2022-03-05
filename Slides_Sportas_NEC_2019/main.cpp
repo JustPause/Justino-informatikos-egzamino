b19111168430cc753cf789d1b5e3ca5753c0af28
Justinas <Justinaslolt@gmail.com>
2022-03-05 12:10:05 +0200

    smole update

---
+++ Slides_Sportas_NEC_2019/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int ilgis=20;
+
+struct Startas
+{
+//char v[i1],p[i1];
+    char vp[ilgis];
+};
+
+int main()
+{
+
+    Startas z;
+    ifstream file_in("U1.txt");
+    int n; //eluciu skaicius
+
+    file_in >> n;
+cout<<n;
+    for (int i =0; i<n; i++)
+    {
+        file_in.get (z.vp,ilgis);
+        file_in>>z.vp[i];
+        cout <<z.vp[i];
+
+
+    }
+    return 0;
+}
+
