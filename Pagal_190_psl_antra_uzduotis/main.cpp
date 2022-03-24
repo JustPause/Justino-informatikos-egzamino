9815f73015d68454d6894a4a0d2bdb0a5e6b2878
Justinas <Justinaslolt@gmail.com>
2022-03-24 12:32:16 +0200

    Pagal 190 Uzduoties puslapi 2

---
+++ Pagal_190_psl_antra_uzduotis/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct Sportininkai{
+
+int sportininku_skaicius=6;
+
+int sportininkai[6];
+
+};
+
+void Skaitimas_is_failo(){
+
+
+}
+
+int main()
+{
+
+ifstream failas_i("U1.txt");
+    cout << "Hello world!" << endl;
+    return 0;
+}
