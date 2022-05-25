3a1653d18944171f863faf4486249b529d88a634
JustPause <justinaslolt@gmail.com>
2022-05-25 23:07:09 +0300

    Ka

--- 2022_04_23_IT_2020_Pirma_Uzduotis_Gėlės/2 užduotis. Varžybos/main.cpp
+++ 2022_04_23_IT_2020_Pirma_Uzduotis_Gėlės/2 užduotis. Varžybos/main.cpp
@@ -3,17 +3,46 @@
 
 using namespace std;
 
-struct Zvejyba {
-int Z_sk;
+struct Zvejyba
+{
+    int Z_sk;
+    char VardasPavarde[20]= {};
+    int Pristatytu_zuvu_skaicius;
+    char ZuviesVardas[20]= {};
+    int Zuvies_mase_gramais;
 };
 
 int main()
 {
-    ifstream file_in ("U2.txt");
+    ifstream file_in("U2.txt");
 
-    Zvejyba[];
+    int Z_sk;
 
-    file_in >>  Z;
+    file_in >> Z_sk;
 
+    Zvejyba Zvejys[Z_sk];
+
+    //cout << Z_sk;
+    //char Vardas[20];
+
+    for(int i=0; i<Z_sk; i++)
+    {
+        file_in.ignore();
+        file_in.get(Zvejys[i].VardasPavarde, 20);
+        file_in >> Zvejys[i].Pristatytu_zuvu_skaicius;
+
+        for(int j=0; j<Zvejys[i].Pristatytu_zuvu_skaicius; j++)
+        {
+            file_in.ignore();
+            file_in.get(Zvejys[i].ZuviesVardas, 20);
+            file_in>>Zvejys[i].Zuvies_mase_gramais;
+        }
+    }
+
+for(int i=0;i<20;i++){
+    cout<<Zvejys[1].VardasPavarde[i];
+}
+
+    file_in.close();
     return 0;
 }
